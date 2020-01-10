7712a34ce6859adf46790ec77e8e1f1953bfd92f
Oana Ujica <48068668+OanaUjica@users.noreply.github.com>
2020-01-10 23:32:07 +0200

    Projects added

---
+++ ProgrammingFundamentals/Electricity_MVCProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Electricity_MVCProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
