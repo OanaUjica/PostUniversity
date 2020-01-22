a725b1c08856549266abab2a324c8cb94a3c2f3d
Oana Ujica <48068668+OanaUjica@users.noreply.github.com>
2020-01-22 11:01:12 +0200

    ElectricityMVC updated with reading from keyboard and printing in table

--- ProgrammingFundamentals/Electricity-MVCProject/View/HeaderTable.cs
+++ ProgrammingFundamentals/Electricity-MVCProject/View/HeaderTable.cs
@@ -10,25 +10,25 @@ namespace Electricity_MVCProject.View
         {
             for (int i = 0; i < numberOfCharacters; i++)
             {
-                Console.WriteLine("=");
+                Console.Write("=");
             }
             Console.WriteLine();
         }
 
         public static void HeaderTable1()
         {
-            var header = "|Country\t \t|Electrical curent (KWh/month)|";
-            Line(46);
+            var header = "|Country\t |Electrical curent (KWh/month)|";
+            Line(48);
             Console.WriteLine(header);
-            Line(46);
+            Line(48);
         }
 
         public static void HeaderTable2()
         {
-            var header = "|Country\t \t|Electrical curent  (KWh/month)| %      |";
-            Line(55);
+            var header = "|Country\t|Electrical curent  (KWh/month)| %      |";
+            Line(57);
             Console.WriteLine(header);
-            Line(55);
+            Line(57);
         }
     }
 }
