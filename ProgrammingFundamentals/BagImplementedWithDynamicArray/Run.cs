03c2ed76813efd970900a5d95b5ccc3830cae408
Oana Ujica <48068668+OanaUjica@users.noreply.github.com>
2020-02-14 18:06:26 +0200

    Set collection implemented with dynamic array added

--- ProgrammingFundamentals/BagImplementedWithDynamicArray/Run.cs
+++ ProgrammingFundamentals/BagImplementedWithDynamicArray/Run.cs
@@ -6,18 +6,33 @@ namespace GenericCollectionsImplementedWithDynamicArray
     {
         static void Main(string[] args)
         {
-
+            // Instantiate the Bag collection.
             Bag<int> collection = new Bag<int>();
 
+            // Add elements in the Bag.
             for (int i = 0; i < 10; i++)
             {
                 collection.Add(i);
             }
 
+            // Print the elements in the Bag.
             foreach (var item in collection)
             {
                 Console.Write($"{item} ");
             }
+            Console.WriteLine();
+
+            // Remove an element and print the elements of the Bag after removal.
+            collection.Remove(7);
+            foreach (var item in collection)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+
+            // Print the numar of elements in the Bag.
+            uint length = collection.Length();
+            Console.WriteLine($"Length: {length}");
         }
     }
 }
